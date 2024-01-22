2ced315e942b12c29bcca08607927bcddfda9aff
Alena59 <asdalena59@gmail.com>
2024-01-22 12:20:53 +0300

    new command import

---
+++ src/shared/helpers/common.ts
@@ -0,0 +1,29 @@
+export function generateRandomValue(min:number, max: number, numAfterDigit = 0) {
+  return +((Math.random() * (max - min)) + min).toFixed(numAfterDigit);
+}
+
+export function getRandomItems<T>(items: T[]):T[] {
+  const startPosition = generateRandomValue(0, items.length - 1);
+  const endPosition = startPosition + generateRandomValue(startPosition, items.length);
+  return items.slice(startPosition, endPosition);
+}
+
+export function getRandomItem<T>(items: T[]):T {
+  return items[generateRandomValue(0, items.length - 1)];
+}
+
+export function makePassword(length:number): string {
+  let result = '';
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const charactersLength = characters.length;
+  let counter = 0;
+  while (counter < length) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    counter += 1;
+  }
+  return result;
+}
+
+export function getErrorMessage(error: unknown): string {
+  return error instanceof Error ? error.message : '';
+}
