b6d12f6c68d5a509a533d6b90f1190ab54ac87cd
Alena59 <asdalena59@gmail.com>
2024-01-29 14:25:55 +0300

    I finished my homework

--- src/shared/types/index.ts
+++ src/shared/types/index.ts
@@ -5,3 +5,4 @@ export * from './offer.type.js';
 export * from './city.type.js';
 export * from './location.type.js';
 export * from './mock-server-data.type.js';
+export * from './component.enum.js';
